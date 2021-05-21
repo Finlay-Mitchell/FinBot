de4e52ba5f9978870223d4f860d9a0cc6815b70c
Finlay-Mitchell <49108303+Finlay-Mitchell@users.noreply.github.com>
2021-05-21 10:30:56 +0100

    Added aiml auto-chatbot code.

--- Modules/Python/Cogs/chatbot.py
+++ Modules/Python/Cogs/chatbot.py
@@ -16,6 +16,29 @@ class chatbot(commands.Cog):
     #     else:
     #         await ctx.reply(aiml_response)
 
+    # YES I NEED TO FIX FOR THIS COG
+    # @bot.event
+    # async def on_message(message):
+    #     if message.author.bot or str(message.channel.id) != "840922321266016286":
+    #         await bot.process_commands(message)
+    #         return
+    #
+    #     if message.content is None:
+    #         return
+    #
+    #     if message.content.startswith(config.prefix):
+    #         return
+    #
+    #     elif 'shutdown' in message.content and message.author.id in config.dev_uids:
+    #         await bot.logout()
+    #
+    #     else:
+    #         aiml_response = bot.aiml_kernel.respond(message.content)
+    #         if aiml_response == '':
+    #             await message.channel.send("I don't have a response for that, sorry.")
+    #         else:
+    #             await message.channel.send(aiml_response)
+
 
 def setup(bot):
     bot.add_cog(chatbot(bot))
